21304c967e9b2d17aa823284969a201f4fad79c9
carlitus0 <miknhodu@gmail.com>
2026-06-11 20:54:54 -0300

    Update db.py

--- db.py
+++ db.py
@@ -2,14 +2,17 @@ import aiosqlite
 
 DB = "bot.db"
 
+
 async def init():
     async with aiosqlite.connect(DB) as db:
-await db.execute("""
-CREATE TABLE IF NOT EXISTS guild_config(
-    guild_id INTEGER PRIMARY KEY,
-    log_channel INTEGER
-)
-""")
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS guild_config(
+            guild_id INTEGER PRIMARY KEY,
+            log_channel INTEGER
+        )
+        """)
+
         await db.execute("""
         CREATE TABLE IF NOT EXISTS warns(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -22,28 +25,26 @@ CREATE TABLE IF NOT EXISTS guild_config(
         await db.commit()
 
 
-async def add_warn(guild_id,user_id,reason):
+async def add_warn(guild_id, user_id, reason):
     async with aiosqlite.connect(DB) as db:
         await db.execute(
             "INSERT INTO warns(guild_id,user_id,reason) VALUES(?,?,?)",
-            (guild_id,user_id,reason)
+            (guild_id, user_id, reason)
         )
         await db.commit()
 
 
-async def get_warns(guild_id,user_id):
+async def get_warns(guild_id, user_id):
     async with aiosqlite.connect(DB) as db:
-
         async with db.execute(
             "SELECT reason FROM warns WHERE guild_id=? AND user_id=?",
-            (guild_id,user_id)
+            (guild_id, user_id)
         ) as cursor:
-
             return await cursor.fetchall()
+
+
 async def set_log_channel(guild_id, channel_id):
-
     async with aiosqlite.connect(DB) as db:
-
         await db.execute(
             """
             INSERT OR REPLACE INTO guild_config
@@ -52,14 +53,11 @@ async def set_log_channel(guild_id, channel_id):
             """,
             (guild_id, channel_id)
         )
-
         await db.commit()
 
 
 async def get_log_channel(guild_id):
-
     async with aiosqlite.connect(DB) as db:
-
         async with db.execute(
             """
             SELECT log_channel
