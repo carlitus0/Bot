1f88868ccd30b239edf39a83b77230cf6cdceb26
carlitus0 <miknhodu@gmail.com>
2026-06-11 18:54:17 -0300

    Update panel.py

--- panel.py
+++ panel.py
@@ -1,25 +1,58 @@
 import discord
 
-class BanModal(discord.ui.Modal, title="Banir usuário"):
-    user_id = discord.ui.TextInput(label="ID do usuário")
-    reason = discord.ui.TextInput(label="Motivo")
 
-    async def on_submit(self, interaction: discord.Interaction):
+class BanModal(discord.ui.Modal,title="Banir Usuário"):
+
+    user_id = discord.ui.TextInput(
+        label="ID do usuário"
+    )
+
+    motivo = discord.ui.TextInput(
+        label="Motivo"
+    )
+
+    async def on_submit(
+        self,
+        interaction: discord.Interaction
+    ):
+
         guild = interaction.guild
-        user = await guild.fetch_member(int(self.user_id.value))
 
-        await user.ban(reason=self.reason.value)
+        try:
 
-        await interaction.response.send_message(
-            f"Usuário {user} banido",
-            ephemeral=True
-        )
+            user = await guild.fetch_member(
+                int(self.user_id.value)
+            )
+
+            await user.ban(
+                reason=self.motivo.value
+            )
+
+            await interaction.response.send_message(
+                "Usuário banido.",
+                ephemeral=True
+            )
+
+        except:
+
+            await interaction.response.send_message(
+                "Erro ao banir.",
+                ephemeral=True
+            )
 
 
 class PanelView(discord.ui.View):
-    def __init__(self):
-        super().__init__(timeout=None)
 
-    @discord.ui.button(label="BANIR", style=discord.ButtonStyle.red)
-    async def ban(self, interaction: discord.Interaction, button: discord.ui.Button):
-        await interaction.response.send_modal(BanModal())
+    @discord.ui.button(
+        label="BAN",
+        style=discord.ButtonStyle.red
+    )
+    async def ban_button(
+        self,
+        interaction: discord.Interaction,
+        button: discord.ui.Button
+    ):
+
+        await interaction.response.send_modal(
+            BanModal()
+        )
