170bf9530e3e89c14ea3dafb0a374dbc63b1d467
carlitus0 <miknhodu@gmail.com>
2026-06-15 18:54:51 -0300

    Refactor bot setup and update intents
    Updated bot intents to use all available intents and added help_command as None. Refactored on_ready event and load_cogs function for improved structure.

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-
 import os
 import asyncio
 
 import discord
-from dotenv import load_dotenv
 from discord.ext import commands
+from dotenv import load_dotenv
 
 import config
 
@@ -12,14 +11,21 @@ load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 
-intents = discord.Intents.default()
-intents.message_content = True
-intents.members = True
+intents = discord.Intents.all()
 
 bot = commands.Bot(
     command_prefix=config.PREFIX,
-    intents=intents
+    intents=intents,
+    help_command=None
 )
+
+
+async def load_cogs():
+    for arquivo in os.listdir("./cogs"):
+        if arquivo.endswith(".py"):
+            await bot.load_extension(
+                f"cogs.{arquivo[:-3]}"
+            )
 
 
 @bot.event
@@ -27,24 +33,8 @@ async def on_ready():
     print(f"Logado como {bot.user}")
 
 
-async def load_cogs():
-
-    if not os.path.exists("./cogs"):
-        return
-
-    for arquivo in os.listdir("./cogs"):
-
-        if arquivo.endswith(".py"):
-
-            await bot.load_extension(
-                f"cogs.{arquivo[:-3]}"
-            )
-
-
 async def main():
-
     async with bot:
-
         await load_cogs()
         await bot.start(TOKEN)
 
