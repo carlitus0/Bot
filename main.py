21d0271d1bcfee374c0ba107dc7d9da34b1a4700
carlitus0 <miknhodu@gmail.com>
2026-06-15 19:31:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import discord
 from discord.ext import commands
 from config import TOKEN, PREFIX
 
+await bot.load_extension("cogs.configuration")
 
 intents = discord.Intents.default()
 intents.message_content = True
