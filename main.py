5f92fd84320e2e8294e60ddd700d0a5b750aa92a
carlitus0 <miknhodu@gmail.com>
2026-06-15 23:52:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
-import sqlite3
+
+intents = discord.Intents.default()
+intents.message_content = True
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
